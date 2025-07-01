33aa015af159b97ab599d8a887241313add679d3
Adrian-Koh <adriankoh216@yahoo.com>
2025-07-01 23:45:17 +0800

    Place ships on gameboard on form completion

--- src/index.js
+++ src/index.js
@@ -2,22 +2,55 @@ import "./styles.css";
 import { populateGrids, setupSquareListeners } from "./board-display";
 import { Player } from "./player";
 
+let player;
+let computer;
+
 document.querySelector("#new-game").addEventListener("click", () => {
   document.querySelector("#new-game-form").style.display = "flex";
 });
 
+document
+  .querySelector("#new-game-form")
+  .addEventListener("submit", (event) => event.preventDefault());
+
 document.querySelector("#start-game").addEventListener("click", () => {
-  // place ships
+  const carrierRow = parseInt(document.querySelector("#carrier-row").value);
+  const carrierCol = parseInt(document.querySelector("#carrier-col").value);
+  const carrierHor = document.querySelector("#carrier-horizontal").checked;
+
+  const battleshipRow = parseInt(
+    document.querySelector("#battleship-row").value,
+  );
+  const battleshipCol = parseInt(
+    document.querySelector("#battleship-col").value,
+  );
+  const battleshipHor = document.querySelector(
+    "#battleship-horizontal",
+  ).checked;
+
+  const cruiserRow = parseInt(document.querySelector("#cruiser-row").value);
+  const cruiserCol = parseInt(document.querySelector("#cruiser-col").value);
+  const cruiserHor = document.querySelector("#cruiser-horizontal").checked;
+
+  const submarineRow = parseInt(document.querySelector("#submarine-row").value);
+  const submarineCol = parseInt(document.querySelector("#submarine-col").value);
+  const submarineHor = document.querySelector("#submarine-horizontal").checked;
+
+  const destroyerRow = parseInt(document.querySelector("#destroyer-row").value);
+  const destroyerCol = parseInt(document.querySelector("#destroyer-col").value);
+  const destroyerHor = document.querySelector("#destroyer-horizontal").checked;
+
+  player = new Player(true);
+  player.placeCarrier([carrierRow, carrierCol], carrierHor);
+  player.placeBattleship([battleshipRow, battleshipCol], battleshipHor);
+  player.placeCruiser([cruiserRow, cruiserCol], cruiserHor);
+  player.placeSubmarine([submarineRow, submarineCol], submarineHor);
+  player.placeDestroyer([destroyerRow, destroyerCol], destroyerHor);
+
+  computer = new Player(false);
+  computer.placeComputerShips();
+
+  document.querySelector("#new-game-form").style.display = "none";
+  populateGrids();
+  setupSquareListeners(player, computer);
 });
-
-const player = new Player(true);
-const computer = new Player(false);
-player.placeCarrier([0, 0], true);
-player.placeBattleship([8, 4], true);
-player.placeCruiser([4, 3], false);
-player.placeSubmarine([4, 8], false);
-player.placeDestroyer([0, 9], false);
-
-computer.placeComputerShips();
-populateGrids();
-setupSquareListeners(player, computer);
